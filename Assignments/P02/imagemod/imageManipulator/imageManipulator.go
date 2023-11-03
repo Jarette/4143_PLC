b63827946d1772deddd211115cf2fca9c7faff12
Jarette Greene <jarettegreene09@gmail.com>
2023-11-02 22:53:26 -0500

    commit

--- Assignments/P02/imagemod/imageManipulator/imageManipulator.go
+++ Assignments/P02/imagemod/imageManipulator/imageManipulator.go
@@ -1,3 +1,26 @@
+/*****************************************************************************
+*                    
+*  Author:           Jarette Greene
+*  Email:            jkgreene0406@my.msutexas.edu / jarettegreene09@gmail.com
+*  Label:            P02
+*  Title:            Baby Steps
+*  Course:           CMPS 4143
+*  Semester:         Fall 2023
+* 
+*  Description:
+*		
+*		imageManipulator package that imports a package from the internet 
+*       with function that allow you to perform simple edits of an image 
+*       in this example we are simply placing a rectangle on an image. This 
+*       all uses Go's "New" naming convention to create Go's version of a 
+*       constructor.
+* 
+*  Usage:
+*       N/A 
+*		
+*  Files:           
+*       N/A
+*****************************************************************************/
 // imagemod/imageManipulator/imageManipulator.go
 
 package imageManipulator
@@ -6,28 +29,42 @@ import (
     "github.com/fogleman/gg"
 )
 
+// Old struct
+
  //ImageManipulator represents an image manipulation tool.
 // type ImageManipulator struct {
 //     Image *gg.Context
 // }
-//updating ImageManipulator strut
+
+
+//updating ImageManipulator struct
 type ImageManipulator struct {
     Image *gg.Context
     ImagePath string // Add a field to store the image path
 }
+
+//Old consturctor 
 
 // NewImageManipulator creates a new ImageManipulator instance.
 // func NewImageManipulator(width, height int) *ImageManipulator {
 //   img := gg.NewContext(width, height)
 //    return &ImageManipulator{Image: img}
 // }
+
+
 // Updating constructor.
+
+//NewImageManipulatorWithImage : finds the image in your system and creates for the program 
+// returns an error if unable to be found 
 func NewImageManipulatorWithImage(imagePath string) (*ImageManipulator, error) {
+    //Creating the path to the image 
     img, err := gg.LoadImage(imagePath)
     if err != nil {
+        //displaying error image 
         return nil, err
     }
 
+    // preparing the image to be chaged and rendered 
     ctx := gg.NewContextForImage(img)
     return &ImageManipulator{Image: ctx, ImagePath: imagePath}, nil
 }
--- Assignments/P02/imagemod/main.go
+++ Assignments/P02/imagemod/main.go
@@ -1,12 +1,48 @@
+/*****************************************************************************
+*                    
+*  Author:           Jarette Greene
+*  Email:            jkgreene0406@my.msutexas.edu / jarettegreene09@gmail.com
+*  Label:            P02
+*  Title:            Baby Steps
+*  Course:           CMPS 4143
+*  Semester:         Fall 2023
+* 
+*  Description:
+*		
+*		This program used to show how to locally manage package files in a  
+*		Go workspace. This program also shows the extensive libraries that  
+*		available in Go Lang by using some basic image Manipulation functions 
+*		import from a package taken from the internet then used to perform
+*		simple image manipulation on a presaved image and resave said image 
+*		with a rectangle drawn over it. This program also displays Go Lang's 
+*		version of constructors.
+* 
+*  Usage:
+*       - make sure you have image saved by approipriate name saved in files
+*		- run go run main.go to run program  
+*		
+*  Files:           
+*      go.mod			:		mod file to handle dependencies
+*	   go.sum			:		controls versioning of different packages
+*	   mustang.jpg		:		Image to be manipulated
+*	   mustang.png 		:		Image after manipulation 
+*	   imageManipulator :		package used to manipulate the saved image
+*****************************************************************************/
+//Main Driver Package
 package main
 
+//necessay imports
 import (
+	// fmt package : allows to input and output from and to the screen respectively 
 	"fmt"
+
+	// myimageapp/imageManipulator: local package that contains methods to draw a rectangle on an image
 	"myimageapp/imageManipulator"
 )
 
 func main() {
-	
+	/**
+	Code that simply creates a PNG of a rectangle 
 	// Only Draws a rectangle on a PNG 
 	// // Create an ImageManipulator instance
 	// im := imageManipulator.NewImageManipulator(800, 600)
@@ -16,7 +52,7 @@ func main() {
 
 	// // Save the image to a file
 	// im.SaveToFile("mustangs.png")
-
+	*/
 
 	// Draws a rectangle on an image pulled from files 
 	// Create an ImageManipulator instance with an existing image
